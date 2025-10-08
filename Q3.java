8e55118e1eb2f65682a9fdfe601b7de33c6f1b9d
Louis Thomazeau-Agullo <166228818+Louis-Yuichi@users.noreply.github.com>
2025-10-08 13:18:35 +0000

    maj

---
+++ Q3.java
@@ -0,0 +1,12 @@
+public class Q3
+{
+    public static void main(String[] args)
+    {
+        int nbEtoiles = Integer.parseInt(args[0]);
+
+        for (int cpt = 0; cpt < nbEtoiles; cpt++)
+        {
+            System.out.print("*");
+        }
+    }
+}
